91037410c9e47e8cad06aa6e1a320467a4c49cf2
MariyahW <149527258+MariyahW@users.noreply.github.com>
2024-08-10 20:42:03 -0400

    update challenge 1 complete

---
+++ docs/text.js
@@ -0,0 +1,61 @@
+
+{
+  accounts: [
+    {
+      account_id: 'ZPbjkW7De4IQj1llPbjMUrEmZwLj7QFeGQZWA',
+      balances: {
+        available: 100,
+        current: 110,
+        iso_currency_code: 'USD',
+        last_updated_datetime: '2024-08-10T00:06:24Z',
+        limit: null,
+        unofficial_currency_code: null
+      },
+      mask: '0000',
+      name: 'Plaid Checking',
+      official_name: 'Plaid Gold Standard 0% Interest Checking',
+      persistent_account_id: '8cfb8beb89b774ee43b090625f0d61d0814322b43bff984eaf60386e',
+      subtype: 'checking',
+      type: 'depository'
+    },
+    {
+      account_id: 'MylZnDqGo8Sre6VVDLexTK5MEQxWlBCL1nakj',
+      balances: {
+        available: 200,
+        current: 210,
+        iso_currency_code: 'USD',
+        last_updated_datetime: '2024-08-10T00:06:24Z',
+        limit: null,
+        unofficial_currency_code: null
+      },
+      mask: '1111',
+      name: 'Plaid Saving',
+      official_name: 'Plaid Silver Standard 0.1% Interest Saving',
+      persistent_account_id: '211a4e5d8361a3afb7a3886362198c7306e00a313b5aa944c20d34b6',
+      subtype: 'savings',
+      type: 'depository'
+    }
+  ],
+  item: {
+    available_products: [
+      'assets',
+      'balance',
+      'signal',
+      'identity',
+      'identity_match',
+      'income_verification',
+      'liabilities',
+      'recurring_transactions',
+      'transfer'
+    ],
+    billed_products: [ 'auth', 'transactions' ],   
+    consent_expiration_time: null,
+    error: null,
+    institution_id: 'ins_128026',
+    item_id: '8N6RoEKVqrHrxR77nXxaTqqBL6oGDvfwrezpB',
+    products: [ 'auth', 'transactions' ],
+    update_type: 'background',
+    webhook: ''
+  },
+  request_id: 'xdrye9HhdonbkbH'
+}
